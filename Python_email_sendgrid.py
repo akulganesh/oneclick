57bf7e2dc350e69e608e82e7a6de2e6a1bf9fd82
akulganesh <46016314+akulganesh@users.noreply.github.com>
2021-04-14 18:03:55 +0530

    Update Python_email_sendgrid.py

--- Python_email_sendgrid.py
+++ Python_email_sendgrid.py
@@ -1,18 +1,35 @@
 import json, urllib.request
-import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
-message = Mail(
-    from_email='aganesh651@gmail.com',
-    to_emails='akulaganesh@yahoo.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
-try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e.message)
+
+#Email function
+def send_email(test):
+ loginuser = "outmail"
+ loginpassword="sSG.my8qYSVtQaaKQgby_2UWDw.BrNxvJXZyAD1elzVUy8qaHIQuCkcs6lfV9Z5OO719Mo"
+ fromaddr="aganesh651@gmail "
+ toaddr = "akulaganesh@yahoo.com"
+ msg = MIMEMultipart()
+ msg['From'] = fromaddr
+ msg['To'] = toaddr
+ msg['Subject'] = "Alert"
+ body = "The list of long running job(s) are " + test
+ msg.attach(MIMEText(body, 'plain'))
+ server = smtplib.SMTP('smtp.sendgrid.net', 587)
+ server.starttls()
+ server.login(loginuser,loginpassword)
+ text = msg.as_string()
+ server.sendmail(fromaddr, toaddr, text)
+ server.quit()
+
+
+link="http://172.27.15.194:8088/ws/v1/cluster/apps?states=RUNNING"
+#Mention your threshold time in milliseconds
+Prescribed_limit=10800000
+with urllib.request.urlopen(link) as response:
+ result=json.loads(response.read().decode('utf8'))
+# Below to parse the json
+for jobs in result['apps']['app']:
+    if jobs['elapsedTime']>prescribed_limit:
+       send_email(str("\nApp Name: {}".format(jobs['name']) +" with Application id: {}".format(jobs['id'])+" running for {} hours".format(round(jobs['elapsedTime']/1000/60/60))))
